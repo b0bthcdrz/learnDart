3b598fe2b00aae598670e623023125a0d64d7849
asilverspoong <asilverspoong@gmail.com>
2024-06-30 05:37:07 +0000

    update on complete class initialization

--- main.dart
+++ main.dart
@@ -1,10 +1,22 @@
-void main(){
-
+void main() {
+  final newStudent = Student(1, 'adit', '7a20242025');
+  print(newStudent);
 }
 
 class Student {
-    int _id;
-    String _name;
-    String _class;
-    
+  int _id = 0;
+  String _name = '';
+  String _class = '';
+
+  Student(this._id, this._name, this._class);
+  Student.anonymous() : this(0, 'anonymous', 'anonymous');
+
+  int get id => _id;
+  String get name => _name;
+  String get className => _class;
+
+  @override
+  String toString() {
+    return 'Student(id=$id, name=$name, class=$className)';
+  }
 }
