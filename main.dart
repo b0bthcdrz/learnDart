7e239cd1e4060c0358401b5985134fab5713975d
asilverspoong <asilverspoong@gmail.com>
2024-07-03 01:06:39 +0000

    this is helped by gemini AI, rewritten from scratch

--- main.dart
+++ main.dart
@@ -1,22 +1,34 @@
-void main() {
-  final newStudent = Student(1, 'adit', '7a20242025');
-  print(newStudent);
-}
-
 class Student {
-  int _id = 0;
-  String _name = '';
-  String _class = '';
+  int _id;
+  String _studentName;
+  int _classID;
 
-  Student(this._id, this._name, this._class);
-  Student.anonymous() : this(0, 'anonymous', 'anonymous');
+  Student({required int id, required String studentName, required int classID}) :
+    this._id = id,
+    this._studentName = studentName,
+    this._classID = classID;
+  
 
   int get id => _id;
-  String get name => _name;
-  String get className => _class;
+
+  String get studentName => _studentName;
+
+  int get classID => _classID;
+
+  set id(int id) => _id = id;
+
+  set studentName(String studentName) => _studentName = studentName;
+
+  set classID(int classID) => _classID = classID;
 
   @override
   String toString() {
-    return 'Student(id=$id, name=$name, class=$className)';
+    return 'Student{id: $_id, studentName: $_studentName, classID: $_classID}';
   }
 }
+
+void main() {
+  Student newStudent = Student(id: 1, studentName: 'John Doe', classID: 10);
+  print(newStudent);
+}
+
